84b29835701b088b72f6a4cd3241df247479fcd6
Heruolin <yyte5555@gmail.com>
2024-09-27 12:48:57 +0800

    Changes

---
+++ src/main/java/com/sca/notesspringboot/NotesSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.sca.notesspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesSpringbootApplication.class, args);
+	}
+
+}
