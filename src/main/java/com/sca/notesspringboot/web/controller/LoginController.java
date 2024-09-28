e84736701f42022406e0098fc81f8a0cbf39aa21
Heruolin <yyte5555@gmail.com>
2024-09-28 15:23:35 +0800

    Changes

--- src/main/java/com/sca/notesspringboot/web/controller/LoginController.java
+++ src/main/java/com/sca/notesspringboot/web/controller/LoginController.java
@@ -1,4 +1,17 @@
 package com.sca.notesspringboot.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.awt.*;
+
+@Controller
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
 }
